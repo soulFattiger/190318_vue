87330bf2b84499f7b519156d3715b9aa34014c7f
soul <soul@qq.com>
2019-08-06 23:17:29 +0800

    静态页面

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -1,17 +1,14 @@
 <template>
   <section class="msite">
     <!--首页头部-->
-    <header class="msite_header">
-      <a class="header_search">
+    <Header title="首页">
+      <span class="header_search" slot="left">
         <i class="iconfont icon-sousuo"></i>
-      </a>
-      <a class="header_title">
-        <span class="header_title_text ellipsis">昌平区北七家宏福科技园(337省道北)</span>
-      </a>
-      <a class="header_login">
+      </span>
+      <span class="header_login" slot="right">
         <span class="header_login_text">登录|注册</span>
-      </a>
-    </header>
+      </span>
+    </Header>
     <!--首页导航-->
     <nav class="msite_nav border-1px">
       <div class="swiper-container">
